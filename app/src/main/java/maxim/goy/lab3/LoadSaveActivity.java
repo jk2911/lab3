89d5e306ba3b0a0d897816a3df410d4efc36c4ab
maxim <maksgoy2911@gmail.com>
2022-10-04 18:30:20 +0300

    add interface in last activity_load_save.xml

--- app/src/main/java/maxim/goy/lab3/LoadSaveActivity.java
+++ app/src/main/java/maxim/goy/lab3/LoadSaveActivity.java
@@ -3,6 +3,8 @@ package maxim.goy.lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import com.google.gson.Gson;
 
 public class LoadSaveActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoadSaveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_save);
+    }
+    public void save(View v){
+
+    }
+    public void load(View v){
+
+    }
+    public void backPage(View v){
+
     }
 }
