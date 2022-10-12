9bb8a4da8569078c4dd46f42d2efd7cd2902849f
Maxim Goy <maksgoy2911@gmail.com>
2022-10-12 21:48:04 +0300

    add UserDataActivity

---
+++ app/src/main/java/maxim/goy/lab3/UserDataActivity.java
@@ -0,0 +1,14 @@
+package maxim.goy.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_data);
+    }
+}
