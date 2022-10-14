d3c53593fd7b926bfcbf9e899e319e78fa01a7a9
Maxim Goy <maksgoy2911@gmail.com>
2022-10-14 19:41:24 +0300

    upload image

--- app/src/main/java/maxim/goy/lab3/UserDataActivity.java
+++ app/src/main/java/maxim/goy/lab3/UserDataActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class UserDataActivity extends AppCompatActivity {
     EditText phone, email, socMedia;
+    ImageView photo;
     Bundle bundle;
+    private final int Pick_image = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,7 @@ public class UserDataActivity extends AppCompatActivity {
 
         phone = findViewById(R.id.phone);
         email = findViewById(R.id.email);
+        photo = findViewById(R.id.imageView);
     }
 
     public void nextPage(View v) {
@@ -46,9 +55,23 @@ public class UserDataActivity extends AppCompatActivity {
     }
 
     public void image(View v) {
-        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
-        startActivity(intent);
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, Pick_image);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        try {
+            final Uri imageUri = imageReturnedIntent.getData();
+            final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+            Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+            photo.setImageBitmap(selectedImage);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
